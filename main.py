a68b4962de3cfdbf2123c4aecc90fe9726f55033
Sumanbhandari69 <60544604+Sumanbhandari69@users.noreply.github.com>
2023-04-15 12:57:40 +0545

    new Change

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ class Face_Recognition_System:
     def __init__(self,root):
         self.root = root
         self.root.geometry("1530x790+0+0")
-        self.root.title("Face Recognition System")
+        self.root.title("Facial Attendance System")
+
+        title_lbl =Label(text="Face Recognition Attendance System")
+        title_lbl.place(x=0,y=0,width=10,height= 1245)
+
+
 
 
 
