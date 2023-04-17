c842848152fe6cc42bbd1ef5b52001e163b7451d
Pravinale <pravinale80@gmail.com>
2023-04-17 21:12:12 +0545

    face recognition

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from tkinter import *
 from tkinter import ttk
 from PIL import Image,ImageTk
 from student import Student
+import os
+import cv2
+import numpy as np
+from tkinter import messagebox
+import mysql.connector
+# import cv2.face
 
 class Face_Recognition_System:
     def __init__(self,root):
@@ -26,10 +32,10 @@ class Face_Recognition_System:
         img1 = img1.resize((250, 250), Image.LANCZOS)
         self.photoimg1 = ImageTk.PhotoImage(img1)
 
-        b1 = Button(bg_img,image=self.photoimg1,command=self.student_details,cursor="hand2")
+        b1 = Button(bg_img,image=self.photoimg1,command=self.student_details(),cursor="hand2")
         b1.place(x= 200,y=100,width= 250,height=250)
 
-        b1_1 = Button(bg_img, text="Students Details",command=self.student_details, cursor="hand2",font=("times new roman",20, "bold"),bg="blue",fg="white")
+        b1_1 = Button(bg_img, text="Students Details",command=self.student_details(  ), cursor="hand2",font=("times new roman",20, "bold"),bg="blue",fg="white")
         b1_1.place(x=200, y=350, width=250, height=40)
 
         # Detect face Button
@@ -37,10 +43,10 @@ class Face_Recognition_System:
         img2 = img2.resize((250, 250), Image.LANCZOS)
         self.photoimg2 = ImageTk.PhotoImage(img2)
 
-        b2 = Button(bg_img, image=self.photoimg2, cursor="hand2")
+        b2 = Button(bg_img,command=self.face_recog, image=self.photoimg2, cursor="hand2")
         b2.place(x=600, y=100, width=250, height=250)
 
-        b2_1 = Button(bg_img, text="Face Detector", cursor="hand2", font=("times new roman", 20, "bold"),bg="blue",fg="white")
+        b2_1 = Button(bg_img,command=self.face_recog, text="Face Detector", cursor="hand2", font=("times new roman", 20, "bold"),bg="blue",fg="white")
         b2_1.place(x=600, y=350, width=250, height=40)
 
         # Attendance Button
@@ -59,10 +65,10 @@ class Face_Recognition_System:
         img4 = img4.resize((250, 250), Image.LANCZOS)
         self.photoimg4 = ImageTk.PhotoImage(img4)
 
-        b4 = Button(bg_img, image=self.photoimg4, cursor="hand2")
+        b4 = Button(bg_img,command=self.train_classifier, image=self.photoimg4, cursor="hand2")
         b4.place(x=200, y=450, width=250, height=250)
 
-        b4_1 = Button(bg_img, text="Train Data", cursor="hand2", font=("times new roman", 20, "bold"),bg="blue",fg="white")
+        b4_1 = Button(bg_img,command=self.train_classifier, text="Train Data", cursor="hand2", font=("times new roman", 20, "bold"),bg="blue",fg="white")
         b4_1.place(x=200, y=700, width=250, height=40)
 
         # Developers Button
@@ -92,6 +98,98 @@ class Face_Recognition_System:
     def student_details(self):
         self.new_window = Toplevel(self.root)
         self.app = Student(self.new_window)
+
+    def train_classifier(self):
+        data_dir = ("data")
+        path = [os.path.join(data_dir,file) for file in os.listdir(data_dir)]
+
+        faces =[]
+        ids =[]
+
+        for image in path:
+            img = Image.open(image).convert('L') #Gray scale
+            imagenp = np.array(img,'uint8')
+            id = int(os.path.split(image)[1].split('.')[1])
+
+            faces.append(imagenp)
+            ids.append(id)
+            cv2.imshow("Training",imagenp)
+            cv2.waitKey(1)==13
+
+        ids = np.array(ids)
+
+        #Train classifier and save
+        clf = cv2.face.LBPHFaceRecognizer_create()
+        clf.train(faces,ids)
+        clf.write("classifier.xml")
+        cv2.destroyAllWindows()
+        messagebox.showinfo("Result","Training datasets completed!!")
+
+
+    #face recognition
+    def face_recog(self):
+        def draw_boundary(img,classifier,scalefactor,minneighbour,color,text,clf):
+            gray_image = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+            features = classifier.detectMultiScale(gray_image,scalefactor,minneighbour)
+
+            coord = []
+            for(x,y,w,h) in features:
+                cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
+                id,predict=clf.predict(gray_image[y:y+h,x:x+w])
+                confidence =int((100*(1-predict/300)))
+
+                conn = mysql.connector.connect(host="localhost", username="root", password="9818913355",
+                                               database="face_recognition")
+                my_cursor = conn.cursor()
+                my_cursor.execute("select Name from student where Student_ID="+str(id))
+                n = my_cursor.fetchone()
+                n = "".join(str(n))
+
+                my_cursor.execute("select Roll from student where Student_ID=" + str(id))
+                r = my_cursor.fetchone()
+                r = "".join(str(r))
+
+                my_cursor.execute("select Dep from student where Student_ID=" + str(id))
+                d = my_cursor.fetchone()
+                d = "".join(str(d))
+
+
+
+                if confidence>77:
+                    cv2.putText(img,f"Roll:{r}",(x,y-55),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
+                    cv2.putText(img, f"Name:{n}", (x, y - 30), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 255, 255), 3)
+                    cv2.putText(img, f"Department:{d}", (x, y - 5), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 255, 255), 3)
+
+                else:
+                    cv2.rectangle(img,(x, y), (x + w, y + h), (0,0,255), 3)
+                    cv2.putText(img, "Unknown Face", (x, y - 5), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 255, 255), 3)
+
+                coord =[x,y,w,h]
+            return coord
+
+        def recognize(img,clf,faceCascade):
+            coord = draw_boundary(img,faceCascade,1.1,10,(255,255,255),"Face",clf)
+            return img
+        face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+        clf=cv2.face.LBPHFaceRecognizer_create()
+        clf.read("classifier.xml")
+
+        video_cap = cv2.VideoCapture(0)
+
+        while True:
+            ret,img = video_cap.read()
+            img = recognize(img,clf,face_cascade)
+            cv2.imshow("Welcome to face recognition",img)
+
+            if cv2.waitKey(1)==13:
+                break
+
+        video_cap.release()
+        cv2.destroyAllWindows()
+
+
+
+
 
 
 
